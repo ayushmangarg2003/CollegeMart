0e727e76a456a4f342ba2293e54530e232b9bc62
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2025-02-15 11:19:40 +0530

    Extra Code removed

--- components/ButtonGradient.js
+++ components/ButtonGradient.js
@@ -1,11 +1,9 @@
-// "use client";
+import React from 'react'
 
-// const ButtonGradient = ({ title = "Gradient Button", onClick = () => {} }) => {
-//   return (
-//     <button className="btn btn-gradient animate-shimmer" onClick={onClick}>
-//       {title}
-//     </button>
-//   );
-// };
+const ButtonGradient = () => {
+  return (
+    <div>ButtonGradient</div>
+  )
+}
 
-// export default ButtonGradient;
+export default ButtonGradient
