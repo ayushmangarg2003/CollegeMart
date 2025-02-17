39bd6b46397aa70c3c6babaef0b50509ce707b51
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2025-02-17 23:06:15 +0530

    Marketplace header done

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,16 +1,37 @@
 import ButtonAccount from "@/components/ButtonAccount";
+import config from "@/config";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/app/icon.png";
 
 export const dynamic = "force-dynamic";
 
-
 export default async function Dashboard() {
   return (
-    <main className="min-h-screen p-8 pb-24">
-      <section className="max-w-xl mx-auto space-y-8">
-        <ButtonAccount />
-        
-        <h1 className="text-3xl md:text-4xl font-extrabold">Marketplace Here</h1>
-      </section>
+    <main className="min-h-screen pb-24">
+      <div className="flex justify-between">
+        <header className="bg-base-200 w-full flex justify-between px-8 py-4">
+          <Link
+            className="flex items-center gap-2 shrink-0 "
+            href="/"
+            title={`${config.appName} hompage`}
+          >
+            <Image
+              src={logo}
+              alt={`${config.appName} logo`}
+              className="w-8"
+              placeholder="blur"
+              priority={true}
+              width={32}
+              height={32}
+            />
+            <span className="font-extrabold text-lg">{config.appName}</span>
+          </Link>
+
+          <ButtonAccount/>
+        </header>
+      </div>
+      <h1 className="text-3xl md:text-4xl font-extrabold">Marketplace Here</h1>
     </main>
   );
 }
